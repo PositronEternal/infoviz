196805932d572b3a84b5f8b826dd4342a43ef073
Jonathan Williams <john.williams@spiersnt.com>
2021-07-01 14:09:03 -0500

    initial draft

--- www/html/index.php
+++ www/html/index.php
@@ -5,11 +5,26 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/infoviz.css">
+  <script src="/js/infoviz.js"></script>
   <title>Hot PHP Apache Mysql!!!</title>
 </head>
 
 <body>
-  Hello World!
+  <ul class="data-node">
+    <h1>Testing here</h1>
+    <ul class="data-node">
+      <h3>Some small section</h3>
+      <li class="data-leaf"><label for="data_leaf[7]">Test 7 with a very long label, we'll see</label><input id="data_leaf[7]" type="text" name="heirarchical_from_nodes[7]" value="test value 7"></li>
+      <li class="data-leaf"><label for="data_leaf[8]">Test 8</label><input id="data_leaf[8]" type="text" name="heirarchical_from_nodes[8]" value="test value 8"></li>
+    </ul>
+    <li class="data-leaf"><label for="data_leaf[0]">Test 1</label><input id="data_leaf[0]" type="text" name="heirarchical_from_nodes[0]" value="test value 1"></li>
+    <li class="data-leaf"><label for="data_leaf[1]">Test 2</label><input id="data_leaf[1]" type="text" name="heirarchical_from_nodes[1]" value="test value 2"></li>
+    <li class="data-leaf"><label for="data_leaf[2]">Test 3</label><input id="data_leaf[2]" type="text" name="heirarchical_from_nodes[2]" value="test value 3"></li>
+    <li class="data-leaf"><label for="data_leaf[3]">Test 4</label><input id="data_leaf[3]" type="text" name="heirarchical_from_nodes[3]" value="test value 4"></li>
+    <li class="data-leaf"><label for="data_leaf[4]">Test 5</label><input id="data_leaf[4]" type="text" name="heirarchical_from_nodes[4]" value="test value 5"></li>
+    <li class="data-leaf"><label for="data_leaf[5]">Test 6</label><input id="data_leaf[5]" type="text" name="heirarchical_from_nodes[5]" value="test value 6"></li>
+  </ul>
 </body>
 
 </html>
