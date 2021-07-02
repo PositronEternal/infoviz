c4312fdce91d2fca0f3f13907a1b36edb31d572c
Jonathan Williams <john.williams@spiersnt.com>
2021-07-01 19:40:37 -0500

    added data-driven html generation

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,62 @@
+<?php
+
+
+$data = [];
+
+$data['title'] = 'Level 1';
+$data['section1'] = ['somedata' => 'lvl2', 'title' => 'Level 2', 'Favorite Color' => 'Blue', 'somesub' => ['more data' => 'lvl3', 'title' => 'Level 3', 'another data' => 'here\'s data']];
+$data['Key 1'] = 'value1';
+$data['Key 2'] = 'Value 2';
+$data['controls'] = [];
+
+
+function build_node($data_node, $parentage = [], $level = 1)
+{
+  $title = $data_node['title'] ?? null;
+  
+  if (!$title) {
+    return;
+  }
+
+  echo '<ul class="data-node"><h' . $level . '>' . $title . '</h' . $level . '>';
+
+  $controls = $data_node['controls'] ?? null;
+
+  foreach (array_filter(
+    array_keys($data_node),
+    function ($key) {
+      return !in_array($key, ['title']);
+    }
+  ) as $key) {
+    $node = $data_node[$key];
+
+    $id_path = implode('_', $parentage);
+    $new_keyname = str_replace(' ', '_', $key);
+    $remaining_path = array_slice($parentage, 1);
+    $name_path = empty($parentage) ? '' :  $parentage[0] . (empty($remaining_path) ? '' : '[' . implode('][', $remaining_path) . ']') . '['. $new_keyname . ']';
+
+    $path_key = $id_path . ($id_path ? '_' : '') . $new_keyname;
+
+    if (is_array($node)) {
+      array_push($parentage, $new_keyname);
+      build_node($node, $parentage, $level + 1);
+      array_pop($parentage);
+    } else {
+
+      if($key == 'controls') {
+        // handle controls building
+      }
+      else {
+        echo '<li class="data-leaf"><label for="' . $path_key .'">' . $key . '</label><input id="' . $path_key . '" type="text" name="'. $name_path .'" value="'. $data_node[$key] . '"></li>';
+      }
+    }
+  }
+
+  echo '</ul>';
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,25 +71,12 @@
 
 <body>
   <form action="">
-    <ul class="data-node">
-      <h1>Testing here</h1>
-      <ul class="data-node">
-        <h3>Some small section</h3>
-        <li class="data-leaf"><label for="data_leaf[7]">Test 7 with a very long label, we'll see</label><input id="data_leaf[7]" type="text" name="heirarchical_from_nodes[7]" value="test value 7"></li>
-        <li class="data-leaf"><label for="data_leaf[8]">Test 8</label><input id="data_leaf[8]" type="text" name="heirarchical_from_nodes[8]" value="test value 8"></li>
-      </ul>
-      <li class="data-leaf"><label for="data_leaf[0]">Test 1</label><input id="data_leaf[0]" type="text" name="heirarchical_from_nodes[0]" value="test value 1"></li>
-      <li class="data-leaf"><label for="data_leaf[1]">Test 2</label><input id="data_leaf[1]" type="text" name="heirarchical_from_nodes[1]" value="test value 2"></li>
-      <li class="data-leaf"><label for="data_leaf[2]">Test 3</label><input id="data_leaf[2]" type="text" name="heirarchical_from_nodes[2]" value="test value 3"></li>
-      <li class="data-leaf"><label for="data_leaf[3]">Test 4</label><input id="data_leaf[3]" type="text" name="heirarchical_from_nodes[3]" value="test value 4"></li>
-      <li class="data-leaf"><label for="data_leaf[4]">Test 5</label><input id="data_leaf[4]" type="text" name="heirarchical_from_nodes[4]" value="test value 5"></li>
-      <li class="data-leaf"><label for="data_leaf[5]">Test 6</label><input id="data_leaf[5]" type="text" name="heirarchical_from_nodes[5]" value="test value 6"></li>
-      <ul class="data-node form-controls-node">
-        <li class="data-leaf"><input type="button" value="Submit Form"></li>
-        <li class="data-leaf"><input type="button" value="Clear Form"></li>
-      </ul>
-    </ul>
+    <?php
+
+    build_node($data);
+
+    ?>
   </form>
 </body>
 
-</html>
+</html><?php
